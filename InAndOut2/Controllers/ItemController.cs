3dce1530ea63cf028238c2d67aca0ae732e80a6a
adriantwardy <adriantwardawski@gmail.com>
2021-04-18 21:10:29 +0200

    Passing Data To The Controller

--- InAndOut2/Controllers/ItemController.cs
+++ InAndOut2/Controllers/ItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InAndOut2.Data;
+using InAndOut2.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,17 @@ namespace InAndOut2.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
         }
     }
 }
