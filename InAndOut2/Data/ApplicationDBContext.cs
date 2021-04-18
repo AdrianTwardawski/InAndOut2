2f0f7991cae3ee4f50fdad6cae9d8b5f08a1a5f2
adriantwardy <adriantwardawski@gmail.com>
2021-04-18 19:50:46 +0200

    Add A Controller For Borrowed Items

--- InAndOut2/Data/ApplicationDBContext.cs
+++ InAndOut2/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InAndOut2.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace InAndOut2.Data
         {
 
         }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
