4a2b256e53a5fd1a4d3d3177391de21e853b68db
adriantwardy <adriantwardawski@gmail.com>
2021-04-19 15:30:36 +0200

    Delete Entry in The Database

--- InAndOut2/Models/Expense.cs
+++ InAndOut2/Models/Expense.cs
@@ -13,7 +13,10 @@ namespace InAndOut2.Models
         public int Id { get; set; }
 
         [DisplayName("Expense Name")]
+        [Required]
         public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Ammount must be greater than 0!")]
         public int Amount { get; set; }
     }
 }
