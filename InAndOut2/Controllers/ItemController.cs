e58013ae642ac51e9452a218b845109d3360e8a4
adriantwardy <adriantwardawski@gmail.com>
2021-04-19 09:58:11 +0200

    Creating The Entry in The Database

--- InAndOut2/Controllers/ItemController.cs
+++ InAndOut2/Controllers/ItemController.cs
@@ -23,9 +23,21 @@ namespace InAndOut2.Controllers
             return View(objList);
         }
 
+        // GET - Create
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
